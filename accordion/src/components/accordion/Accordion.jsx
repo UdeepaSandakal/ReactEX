af039919e9d3ed23758f267229e484a214e95138
udeepasd <udeepasd@gmail.com>
2024-04-02 13:46:20 +0530

    single-selection-accordion

---
+++ accordion/src/components/accordion/Accordion.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import data from "./data";
+import "./styles.css";
+
+const Accordion = () => {
+  const [selected, setSelected] = useState(null);
+
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+  return (
+    <div className="wrapper">
+      <h3 className="header">Single Selection Accordion</h3>
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
+            </div>
+          ))
+        ) : (
+          <div>No data Found !</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
